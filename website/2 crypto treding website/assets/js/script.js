f71c2030deb3282e7c123ba9fe2dd6cf22ef8df6
Baturpcu <batusalkim11@gmail.com>
2023-02-16 03:44:23 +0300

    update

--- website/2 crypto treding website/assets/js/script.js
+++ website/2 crypto treding website/assets/js/script.js
@@ -26,6 +26,8 @@ const navbar = document.querySelector("[data-navbar]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const navToggler = document.querySelector("[data-nav-toggler]");
 
+
+
 const toggleNavbar = function () {
   navbar.classList.toggle("active");
   navToggler.classList.toggle("active");
