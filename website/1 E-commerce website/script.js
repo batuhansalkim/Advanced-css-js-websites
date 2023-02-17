f47c365e10dd21ecf9ab8b3991b4b3d58a6cb937
Baturpcu <batusalkim11@gmail.com>
2023-02-18 02:03:39 +0300

    update

---
+++ website/1 E-commerce website/script.js
@@ -0,0 +1,17 @@
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close");
+if(bar){
+    bar.addEventListener("click",()=>{
+        nav.classList.add("active");
+    });
+    close.addEventListener("click",()=>{
+        nav.classList.toggle("active");
+    })
+
+}
+
+
+
+
+
