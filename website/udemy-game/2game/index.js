90592c6a6ec8029197ec5e74d9f2b6b0fa9ae57e
Baturpcu <batusalkim11@gmail.com>
2023-03-06 20:24:44 +0300

    update

--- website/udemy-game/2game/index.js
+++ website/udemy-game/2game/index.js
@@ -5,45 +5,7 @@ for(var i=0; i<numberOfDrumButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click",function(){
     
     var buttonInnerHTML = this.innerHTML;
-    switch(buttonInnerHTML){
-      case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "s":
-        var tom3 = new Audio('sounds/tom-3.mp3');
-        tom3.play();
-        break;
-
-      case "d":
-        var tom4 = new Audio('sounds/tom-4.mp3');
-        tom4.play();
-        break;
-
-      case "j":
-        var snare = new Audio('sounds/snare.mp3');
-        snare.play();
-        break;
-
-      case "k":
-        var crash = new Audio('sounds/crash.mp3');
-        crash.play();
-        break;
-
-      case "l":
-        var kick = new Audio('sounds/kick-bass.mp3');
-        kick.play();
-        break;
-
-      default:
-        
-    }
+    makeSound(buttonInnerHTML);
   })
 }
 
@@ -51,6 +13,47 @@ document.addEventListener("keypress",function(event){
   console.log(event);
 })
 
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+    default:
+
+  }
+}
 // // var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 // var numberOfDrumButtons = document.querySelectorAll(".drum").length; 
