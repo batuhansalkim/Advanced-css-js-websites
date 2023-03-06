9d1176fdea20fab5a019aa7eee9079514f16673a
Baturpcu <batusalkim11@gmail.com>
2023-03-06 22:28:20 +0300

    update

--- website/udemy-game/2game/index.js
+++ website/udemy-game/2game/index.js
@@ -61,7 +61,12 @@ function makeSound(key){
 }
 
 function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
 
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
 }
 
 
