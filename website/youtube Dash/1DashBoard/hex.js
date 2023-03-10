c6153105dcd82fc0182ddca1ec4e732ed169a65a
Baturpcu <batusalkim11@gmail.com>
2023-03-10 03:57:11 +0300

    update

--- website/youtube Dash/1DashBoard/hex.js
+++ website/youtube Dash/1DashBoard/hex.js
@@ -3,7 +3,10 @@ let toggle = document.querySelector(".toggle");
 let navigation = document.querySelector(".navigation");
 let main = document.querySelector(".main");
 
-
+toggle.onclick = function(){
+    navigation.classList.toggle("active");
+    main.classList.toggle("active");
+}
 
 
 function activeLink(){
