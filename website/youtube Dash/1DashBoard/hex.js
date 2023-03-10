11a8cef79b201b8dfe347c1ddc465ade9b222e0a
Baturpcu <batusalkim11@gmail.com>
2023-03-10 03:47:13 +0300

    update

--- website/youtube Dash/1DashBoard/hex.js
+++ website/youtube Dash/1DashBoard/hex.js
@@ -1,7 +1,33 @@
 let list = document.querySelectorAll(".navigation li");
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+
+
+
 function activeLink(){
     list.forEach((item)=>
     item.classList.remove("hovered"));
     this.classList.add("hovered");
 }
 list.forEach((item)=>item.addEventListener("mouseover",activeLink));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
