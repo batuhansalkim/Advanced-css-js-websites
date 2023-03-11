fcba8bf071911dc9b80ba555d73fb535ae3b85d7
Baturpcu <batusalkim11@gmail.com>
2023-03-12 02:06:07 +0300

    update

--- website/udemygame/game.js
+++ website/udemygame/game.js
@@ -15,8 +15,27 @@
 //Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
 
 //8. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+
+//9. At the top of the game.js file, create a new empty array with the name userClickedPattern.
+
+//10. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+
+//11. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+
+//12. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+
 var buttonColours = ["red","blue","green","yellow"];
 var gamePattern = [];
+
+var userClickedPattern = [];
+
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+})
+
 
 function nextSequence(){
 
@@ -28,7 +47,8 @@ function nextSequence(){
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var audio =
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
 
 
