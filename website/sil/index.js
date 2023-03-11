12be08f17b939356da5ca1e03fa9a4fc6c809de0
Baturpcu <batusalkim11@gmail.com>
2023-03-11 03:51:32 +0300

    update

---
+++ website/sil/index.js
@@ -0,0 +1,14 @@
+// var superKahraman = require("superheroes");
+
+// var benimsuperkahramanim = superKahraman.random();
+
+// console.log(benimsuperkahramanim);
+
+const supervillains = require('supervillains');
+
+supervillains.all;
+//=> ['Abattoir', 'Able Crown', …]
+
+var batu = supervillains.random();
+console.log(batu);
+
